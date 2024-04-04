267d6751aded74af90d8efebd4572823cd7d5e0a
cassidy <cassidykovell@Cassidys-iMac.lan>
2024-04-04 09:42:21 -0700

    added seeds and the seed.js as well as corrected the models

--- models/index.js
+++ models/index.js
@@ -3,21 +3,26 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-})
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-})
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: 'user_id'
+});
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
 
 module.exports = { User, Post, Comment };
